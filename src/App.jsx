73b578682ec34944631c675c60dcc0c8abcf7465
jcarlos2n <eljcarlos2n@gmail.com>
2022-12-03 11:59:04 +0100

    feat: create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
+
+import { BrowserRouter } from 'react-router-dom'
+import Header from './components/Header/Header'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+
+import './Header.css'
+
+const Header = () => {
+
+    return(
+        <div className='headerBox'>
+            <h1>El_hasky</h1>
+        </div>
+    )
+
+}
+
+export default Header
