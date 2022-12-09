2ec9560586755b03c141171db1c35d11a86dcd8c
jocan <eljcarlos2n@gmail.com>
2022-12-09 22:17:55 +0100

    feat: header update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 
 import './Header.css'
+import logo from "../../assets/el_hasky_2.png"
 
 const Header = () => {
 
-    return(
+    return (
         <div className='headerBox'>
-            <h1>El_hasky</h1>
+            <div className='logoBox'>
+                <img className='logoHeader' src={logo} alt="Logo" />
+                <h1>EL HASKY</h1>
+            </div>
         </div>
     )
 
