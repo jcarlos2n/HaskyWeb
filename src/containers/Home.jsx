3d01c1dfc1fc78a54de6812484eba3383588e989
jocan <eljcarlos2n@gmail.com>
2022-12-11 13:58:23 +0100

    changes links and add logo png

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -11,29 +11,29 @@ const Home = () => {
     return (
         <div className='homeBox'>
             <div className="socialsBox">
-                <div className="socials">
+                <a href="" className="socials">
                     <img className="twitch" src={twitch} alt="TWITCH" />
-                    <a href="">TWITCH</a>
-                </div>
-                <div className="socials">
+                    <h3 className="socialName" href="">TWITCH</h3>
+                </a>
+                <a href="" className="socials">
                     <img className="twitch" src={insta} alt="INSTAGRAM" />
-                    <a href="">INSTAGRAM</a>
-                </div>
-                <div className="socials">
-                    <img src="" alt="YOUTUBE" />
-                    <a href="">YOUTUBE</a>
-                </div>
-                <div className="socials">
-                    <img src="" alt="TIK TOK" />
-                    <a href="">TIK TOK</a>
-                </div>
-                <div className="socials">
-                    <img src="" alt="TWITTER" />
-                    <a href="">TWITTER</a>
-                </div>
+                    <h3 className="socialName" href="">INSTAGRAM</h3>
+                </a>
+                <a href="" className="socials">
+                    <img className="twitch" src={youtube} alt="YOUTUBE" />
+                    <h3 className="socialName" href="">YOUTUBE</h3>
+                </a>
+                <a href="" className="socials">
+                    <img className="twitch" src={tik} alt="TIK TOK" />
+                    <h3 className="socialName" href="">TIK TOK</h3>
+                </a>
+                <a href="" className="socials">
+                    <img className="twitch" src={twitter} alt="TWITTER" />
+                    <h3 className="socialName" href="">TWITTER</h3>
+                </a>
             </div>
             <div className="video">
-            <iframe width="800" height="450" src="https://www.youtube.com/embed/bBvicg_yeJc?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                <iframe width="800" height="450" src="https://www.youtube.com/embed/bBvicg_yeJc?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
             </div>
         </div>
     )
