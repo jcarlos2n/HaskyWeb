4d3f468779e491dae7f50fdcb7f69b6029aa1407
jocan <eljcarlos2n@gmail.com>
2022-12-11 01:27:53 +0100

    feat: add video autoplay

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -12,28 +12,28 @@ const Home = () => {
         <div className='homeBox'>
             <div className="socialsBox">
                 <div className="socials">
-                    <img className="twitch" src={twitch} alt="" srcset="" />
+                    <img className="twitch" src={twitch} alt="TWITCH" />
                     <a href="">TWITCH</a>
                 </div>
                 <div className="socials">
-                    <img className="twitch" src={insta} alt="" srcset="" />
+                    <img className="twitch" src={insta} alt="INSTAGRAM" />
                     <a href="">INSTAGRAM</a>
                 </div>
                 <div className="socials">
-                    <img src="" alt="" srcset="" />
+                    <img src="" alt="YOUTUBE" />
                     <a href="">YOUTUBE</a>
                 </div>
                 <div className="socials">
-                    <img src="" alt="" srcset="" />
+                    <img src="" alt="TIK TOK" />
                     <a href="">TIK TOK</a>
                 </div>
                 <div className="socials">
-                    <img src="" alt="" srcset="" />
+                    <img src="" alt="TWITTER" />
                     <a href="">TWITTER</a>
                 </div>
             </div>
             <div className="video">
-            <iframe width="600" height="340" src="https://www.youtube.com/embed/bBvicg_yeJc?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="800" height="450" src="https://www.youtube.com/embed/bBvicg_yeJc?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
             </div>
         </div>
     )
